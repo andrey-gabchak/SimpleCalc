028c16f8c17b4073fe395a89bb3f223543ecdd6c
Andrey.Gabchak <andrey.gabchak@gmail.com>
2016-09-23 18:18:39 +0300

    Added sqrt operation

---
+++ src/main/java/Operations/Sqrt.java
@@ -0,0 +1,18 @@
+package Operations;
+
+public class Sqrt implements UnaryOperation {
+    @Override
+    public double getResult(double value) {
+        return Math.sqrt(value);
+    }
+
+    @Override
+    public String getOperator() {
+        return "s";
+    }
+
+    @Override
+    public int getPriority() {
+        return 3;
+    }
+}
