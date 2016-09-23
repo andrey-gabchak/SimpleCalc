fbaf3ed471cf57b48922fea934e1c34b063a0165
Andrey.Gabchak <andrey.gabchak@gmail.com>
2016-09-23 18:15:08 +0300

    Added list of unary operators

--- src/main/java/Arithmetic.java
+++ src/main/java/Arithmetic.java
@@ -1,26 +1,34 @@
 import Operations.AllOperations;
-import Operations.Operation;
+import Operations.BinaryOperation;
+import Operations.UnaryOperation;
 
 import java.util.List;
 import java.util.Stack;
 
 
 class Arithmetic {
-    private final List<Operation> operationsList = AllOperations.getAllOperations().getOperationsList();
+    private final List<BinaryOperation> binaryOperations = AllOperations.getAllOperations().getBinaryOperations();
+    private final List<UnaryOperation> unaryOperations = AllOperations.getAllOperations().getUnaryOperations();
 
     private Stack<Double> stackResult = new Stack<>();
 
 
     protected Double calculation(String expression) {
         Parser parser = new Parser();
-        Stack<String> stack = parser.parsing(expression);
-        for (String s : stack) {
+        Stack<String> stackPRN = parser.parsing(expression);
+
+        for (String s : stackPRN) {
             try {
                 stackResult.push(new Double(s));
             } catch (NumberFormatException e) {
-                for (Operation operation : operationsList) {
-                    if (s.equals(operation.getOperator())) {
-                        stackResult.push(operation.getResult(stackResult.pop(), stackResult.pop()));
+                for (BinaryOperation binaryOperation : binaryOperations) {
+                    if (s.equals(binaryOperation.getOperator())) {
+                        stackResult.push(binaryOperation.getResult(stackResult.pop(), stackResult.pop()));
+                    }
+                }
+                for (UnaryOperation unaryOperation : unaryOperations) {
+                    if (s.equals(unaryOperation.getOperator())) {
+                        stackResult.push(unaryOperation.getResult(stackResult.pop()));
                     }
                 }
             }
